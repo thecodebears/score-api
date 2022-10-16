634e3d782a3637ac4799595af9832fc8818f7c03
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 07:44:50 +0300

    Initialized connection module, with extra imports

--- src/models/connection/connection.module.ts
+++ src/models/connection/connection.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { ConnectionService } from './connection.service';
 import { Connection } from './connection.entity';
+import { ConnectionController } from './connection.controller';
+import { AccountModule } from '../account/account.module';
 
 
 @Module({
-    imports: [ TypeOrmModule.forFeature([ Connection ]) ],
+    imports: [
+        TypeOrmModule.forFeature([ Connection ]),
+        AccountModule,
+        HttpModule
+    ],
     providers: [ ConnectionService ],
-    controllers: [],
-    exports: [ ConnectionService ]
+    controllers: [ ConnectionController ]
 })
 export class ConnectionModule {}
