e707b28efbbc0f32a62b0ad88e8b87b35241a16b
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 18:39:14 +0300

    Renamed to environment.ts

---
+++ environment.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+
+
+namespace environment {
+    export const scope: string = process.env.scope;
+    export const domain: string = process.env.domain;
+    export const port: number = parseInt(process.env.port);
+
+    export namespace credentials {
+        export const discord = {
+            clientId: process.env.discordClientId,
+            clientSecret: process.env.discordClientSecret,
+            callbackUrl: `http://${ process.env.scope === 'DEV' ? `localhost:${port}` : domain }/${process.env.discordCallbackUrl}`
+        };
+    }
+
+    export namespace security {
+        export const jwt = {
+            secret: process.env.jwtSecret
+        };
+        export const session = {
+            secret: process.env.sessionSecret
+        };
+    }
+}
+
+console.log(environment.credentials.discord);
+
+export default environment;
