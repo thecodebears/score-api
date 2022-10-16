c937048b0196954ee2888a11e410e4cf2085d1e1
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 18:41:29 +0300

    Removed unused declarations

--- src/models/connection/connection.types.ts
+++ src/models/connection/connection.types.ts
@@ -1,24 +1,7 @@
-export type ConnectionType = 'vk' | 'google' | 'discord';
+export type ConnectionPlatform = 'vk' | 'google' | 'discord';
 
-export enum ConnectionTypes {
+export enum ConnectionPlatforms {
     VK='vk',
     Discord='discord',
     Google='google'
 }
-
-export type ConnectionRequest = {
-    type: ConnectionType,
-    key: string,
-    placeholderName: string,
-    accountId?: string
-};
-
-export type ConnectionPair = {
-    account: string,
-    connection: string
-};
-
-export type DiscordAccount = {
-    id: string,
-    username: string
-};
