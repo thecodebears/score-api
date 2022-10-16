164fe90f004059716b46e5645082aec1ddbcf4be
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 18:44:54 +0300

    Fixed imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConnectionModule } from './models/connection/connection.module';
+import { AuthModule } from './auth/auth.module';
+import { ApplicationModule } from './models/application/application.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import ormConfig from 'ormconfig';
@@ -9,10 +11,11 @@ import ormConfig from 'ormconfig';
 
 @Module({
     imports: [
-        ConnectionModule,
+        AuthModule,
+        ApplicationModule,
         TypeOrmModule.forRoot(ormConfig)
     ],
     controllers: [ AppController ],
-    providers: [ AppService ],
+    providers: [ AppService ]
 })
 export class AppModule {}
