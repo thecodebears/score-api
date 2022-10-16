959b70b9608b5d06ef45190c1ad21758be2cbe9c
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 18:39:42 +0300

    Linked session and passport's middlewares

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { config } from 'config';
+import env from 'environment';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    // Powering by Fastify
+    // const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+    // Powering by Express
+    const app = await NestFactory.create(AppModule);
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.use(session({
+        secret: env.security.session.secret,
+        resave: false,
+        saveUninitialized: false
+    }));
 
-    await app.listen(config.port);
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    await app.listen(env.port);
 }
 
 bootstrap();
