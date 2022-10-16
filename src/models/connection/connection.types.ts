d83c5d8e85988368ad1df27cad324eb32e91df03
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 07:45:20 +0300

    Initialized connection types

--- src/models/connection/connection.types.ts
+++ src/models/connection/connection.types.ts
@@ -1,1 +1,24 @@
 export type ConnectionType = 'vk' | 'google' | 'discord';
+
+export enum ConnectionTypes {
+    VK='vk',
+    Discord='discord',
+    Google='google'
+}
+
+export type ConnectionRequest = {
+    type: ConnectionType,
+    key: string,
+    placeholderName: string,
+    accountId?: string
+};
+
+export type ConnectionPair = {
+    account: string,
+    connection: string
+};
+
+export type DiscordAccount = {
+    id: string,
+    username: string
+};
