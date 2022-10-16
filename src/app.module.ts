979368930f0f16cebd11e69f35f950bb526105d9
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 07:43:16 +0300

    Imported ConnectionModule and ORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConnectionModule } from './models/connection/connection.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import ormConfig from 'ormconfig';
 
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        ConnectionModule,
+        TypeOrmModule.forRoot(ormConfig)
+    ],
+    controllers: [ AppController ],
+    providers: [ AppService ],
 })
 export class AppModule {}
