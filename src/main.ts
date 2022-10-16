994a6adf275ebbc5a1cd93a76788e231636a6002
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 07:46:09 +0300

    Using default global pipe, port is in config now

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { config } from 'config';
 
 
 async function bootstrap() {
     const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-    await app.listen(3000);
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+    await app.listen(config.port);
 }
 
 bootstrap();
