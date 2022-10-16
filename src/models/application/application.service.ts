54339b1c34f87518e415d6f2478c2777bfb0831e
anore <66754841+codelumen@users.noreply.github.com>
2022-10-16 18:43:56 +0300

    Added application authorization

--- src/models/application/application.service.ts
+++ src/models/application/application.service.ts
@@ -3,11 +3,29 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Application } from './application.entity';
 import { EntityService } from '../base.service';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class ApplicationService extends EntityService<Application> {
-    constructor(@InjectRepository(Application) protected readonly repository: Repository<Application>) {
+    constructor(
+        @InjectRepository(Application) protected readonly repository: Repository<Application>,
+        private jwtService: JwtService
+    ) {
         super();
+    }
+
+    public async authorize(id: string): Promise<string> {
+        let application = await this.findOneBy({ id });
+        if (!application) return '';
+
+        let payload = {
+            scope: 'application',
+            id: application.id,
+            permissions: application.permissions,
+            timestamp: Date.now()
+        };
+
+        return this.jwtService.sign(payload);
     }
 }
