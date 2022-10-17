35bea12307c4032448b6a6346901755251ef082c
anore <66754841+codelumen@users.noreply.github.com>
2022-10-17 21:52:03 +0300

    Defined registration response type

--- src/models/account/account.types.ts
+++ src/models/account/account.types.ts
@@ -1,6 +1,13 @@
+import { Account } from "./account.entity";
+
 type ProductId = string;
 type Count = number;
 
 export type Cart = {
     [key: ProductId]: Count
 };
+
+export interface RegistrationResponse {
+    account: Account,
+    token: string
+}
