e9fd9d1ae2590de3ddd5002eef58f134ec29bc2d
anore <66754841+codelumen@users.noreply.github.com>
2022-10-17 21:53:31 +0300

    Removed migrated method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,54 +1,32 @@
-import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 import { Account } from '../models/account/account.entity';
 import { AccountService } from '../models/account/account.service';
 import { ConnectionService } from '../models/connection/connection.service';
 import { ConnectionPlatform } from '../models/connection/connection.types';
-import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
     constructor(
-        private accountService: AccountService,
         private connectionService: ConnectionService,
-        private jwtService: JwtService
+        private accountService: AccountService
     ) {}
-
-    /*
-     * Grants JWT token for access.
-     */
-    public authorize(account: Account): string {
-        let payload = {
-            scope: 'account',
-            id: account.id,
-            timestamp: Date.now()
-        };
-
-        return this.jwtService.sign(payload);
-    }
-
-    public async register(name: string, password: string): Promise<Account> {
-        let passwordHash = bcrypt.hashSync(password, 10);
-
-        return this.accountService.create({ name, password: passwordHash });
-    }
 
     /*
      * Used to authorization from trusted platforms.
      */
     public async connect(platform: ConnectionPlatform, key: string, name: string): Promise<Account> {
         let connection = await this.connectionService.findOneBy({ platform, key });
-        let account: Account;
 
         if (!connection) {
             let password = Math.random().toString(16).slice(2);
-            account = await this.register(name, password);
-            await this.connectionService.create({ platform: 'discord', key, account: account.id });
-        } else {
-            account = await this.accountService.findOneBy({ id: connection.account });
-        }
+            let { account } = await this.accountService.register(name, password);
 
-        return account;
+            await this.connectionService.create({ platform: 'discord', key, account: account.id });
+
+            return account;
+        } else {
+            return this.accountService.findOneBy({ id: connection.account });
+        }
     }
 }
