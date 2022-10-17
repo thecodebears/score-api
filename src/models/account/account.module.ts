c563edf212877e617a42a63ec890770bc2002584
anore <66754841+codelumen@users.noreply.github.com>
2022-10-17 21:51:37 +0300

    Added JWT module import

--- src/models/account/account.module.ts
+++ src/models/account/account.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import {Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountService } from './account.service';
 import { Account } from './account.entity';
+import { JwtModule } from '@nestjs/jwt';
+import env from 'environment';
 
 
+@Global()
 @Module({
-    imports: [ TypeOrmModule.forFeature([ Account ]) ],
+    imports: [
+        TypeOrmModule.forFeature([ Account ]),
+        JwtModule.register({
+            secret: env.security.jwt.secret
+        })
+    ],
     providers: [ AccountService ],
     controllers: [],
     exports: [ AccountService ]
