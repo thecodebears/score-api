069ce767fab4777018b006a92d16e3ee889b6b0c
Laaguini <vovanlihoi@gmail.com>
2022-10-17 21:31:24 +0300

    + imports fix

--- src/models/account/account.module.ts
+++ src/models/account/account.module.ts
@@ -3,10 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller'
 import { Account } from './account.entity';
-
+import { JwtModule } from '@nestjs/jwt';
+import env from 'environment';
 
 @Module({
-    imports: [ TypeOrmModule.forFeature([ Account ]) ],
+    imports: [
+        TypeOrmModule.forFeature([ Account ]),
+        JwtModule.register({
+            secret: env.security.jwt.secret
+        })
+    ],
     providers: [ AccountService ],
     controllers: [ AccountController ],
     exports: [ AccountService ]
