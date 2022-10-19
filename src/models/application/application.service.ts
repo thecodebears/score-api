4b2b1c9a1d6495bb1a15b38308bc447afe44dc33
anore <66754841+codelumen@users.noreply.github.com>
2022-10-19 22:38:18 +0300

    Naming fix, added new method

--- src/models/application/application.service.ts
+++ src/models/application/application.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Application } from './application.entity';
-import { EntityService } from '../base.service';
 import { JwtService } from '@nestjs/jwt';
+import { ModelService } from "../model.service";
+import { NewActionResponse } from "./application.types";
 
 
 @Injectable()
-export class ApplicationService extends EntityService<Application> {
+export class ApplicationService extends ModelService<Application> {
     constructor(
         @InjectRepository(Application) protected readonly repository: Repository<Application>,
         private jwtService: JwtService
@@ -15,10 +16,18 @@ export class ApplicationService extends EntityService<Application> {
         super();
     }
 
-    public async authorize(id: string): Promise<string> {
-        let application = await this.findOneBy({ id });
-        if (!application) return '';
+    public async new(name: string, description: string, permissions: string[]): Promise<NewActionResponse> {
+        try {
+            let application = await this.create({ name, description, permissions });
+            let token = this.authorize(application);
 
+            return { application, token };
+        } catch(e) {
+            return null;
+        }
+    }
+
+    public authorize(application: Application): string {
         let payload = {
             scope: 'application',
             id: application.id,
