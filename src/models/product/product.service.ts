c0c4a17241d6604e4f23d96e81b52162126ffedc
anore <66754841+codelumen@users.noreply.github.com>
2022-10-19 22:40:06 +0300

    FUCK, ANOTHER ONE

--- src/models/product/product.service.ts
+++ src/models/product/product.service.ts
@@ -2,11 +2,11 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Product } from './product.entity';
-import { EntityService } from '../base.service';
+import { ModelService } from '../model.service';
 
 
 @Injectable()
-export class ProductService extends EntityService<Product> {
+export class ProductService extends ModelService<Product> {
     constructor(@InjectRepository(Product) protected readonly repository: Repository<Product>) {
         super();
     }
