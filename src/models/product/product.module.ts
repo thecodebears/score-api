ace1c0efe048df2ea5b6a1a8731b927b92c3c123
anore <66754841+codelumen@users.noreply.github.com>
2022-10-20 00:50:16 +0300

    Added product controller to controllers

--- src/models/product/product.module.ts
+++ src/models/product/product.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
 import { Product } from './product.entity';
+import { ProductController } from './product.controller';
 
 
 @Module({
     imports: [ TypeOrmModule.forFeature([ Product ]) ],
     providers: [ ProductService ],
-    controllers: [],
+    controllers: [ ProductController ],
     exports: [ ProductService ]
 })
 export class ProductModule {}
