37709a7ee6a2e9842a98e4f0d43312f7d86d8403
anore <66754841+codelumen@users.noreply.github.com>
2022-10-20 00:50:30 +0300

    Declared product create request type

--- src/models/product/product.types.ts
+++ src/models/product/product.types.ts
@@ -1,3 +1,6 @@
+import { Transform } from "class-transformer";
+import {IsBoolean, IsJSON, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 type ProductFeatureLabel = string;
 
 type ProductFeatureValue = string;
@@ -5,3 +8,40 @@ type ProductFeatureValue = string;
 export type ProductFeatures = {
     [key: ProductFeatureLabel]: ProductFeatureValue
 };
+
+export class ProductCreateRequest {
+    @IsNotEmpty()
+    @IsString()
+    label: string;
+
+    @IsNotEmpty()
+    @IsString()
+    sublabel: string;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+    @IsNotEmpty()
+    @IsString()
+    category: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Transform(({ value }) => parseInt(value))
+    price: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Transform(({ value }) => parseInt(value))
+    quantity: number;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    @Transform(({ value }) => value === 'true')
+    delivered: boolean;
+
+    @IsNotEmpty()
+    @IsJSON()
+    features: ProductFeatures;
+}
