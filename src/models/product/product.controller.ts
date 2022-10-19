b1ecb6ebcc422a5a6cbcbfa8408d56b028a3c243
anore <66754841+codelumen@users.noreply.github.com>
2022-10-19 23:52:12 +0300

    Implemented product controller

---
+++ src/models/product/product.controller.ts
@@ -0,0 +1,70 @@
+import { Controller, Get, HttpException, Post, Query, UseGuards, Response } from "@nestjs/common";
+import { ApplicationJwtGuard } from "../../guard/jwt.guard";
+import { ModelCreateRequest, ModelDeleteRequest, ModelGetRequest, ModelSearchRequest, ModelUpdateRequest } from "../model.types";
+import { Product } from "./product.entity";
+import { ProductService } from "./product.service";
+import { Permissions } from "../../decorators/permissions.decorator";
+
+
+@Controller()
+export class ProductController {
+    constructor(
+        private productService: ProductService
+    ) {}
+
+    @Get()
+    @Permissions('product.get')
+    @UseGuards(ApplicationJwtGuard)
+    public async get(@Query() { id, fields }: ModelGetRequest) {
+        const product = await this.productService.findOneBy({ id });
+        if (!product) throw new HttpException('Product not found.', 404);
+        return fields ? product.pick(fields?.split(/\,/g)) : product;
+    }
+
+    @Post('create')
+    @Permissions('product.create')
+    @UseGuards(ApplicationJwtGuard)
+    public async create(@Query() fields: ModelCreateRequest<Product>) {
+        return this.productService.create(fields);
+    }
+
+    @Post('search')
+    @Permissions('product.search')
+    @UseGuards(ApplicationJwtGuard)
+    public async search(@Query() { id, ...searchFields }: ModelSearchRequest<Product>) {
+        const products = await this.productService.findBy(searchFields);
+        if (!products.length) throw new HttpException('No results.', 404);
+        return { products };
+    }
+
+    @Post('update')
+    @Permissions('product.update')
+    @UseGuards(ApplicationJwtGuard)
+    public async update(
+        @Query() { id, ...overrideFields }: ModelUpdateRequest<Product>,
+        @Response({ passthrough: true }) res
+    ) {
+        let product = await this.productService.findOneBy({ id });
+        if (!product) throw new HttpException('product not found.', 404);
+
+        let updateResult = await this.productService.update(product, overrideFields);
+        if (!updateResult) throw new HttpException('Failed to update product. Check your parameters, it may be incorrect.', 500);
+
+        res.status(200);
+    }
+
+    @Post('delete')
+    @Permissions('product.delete')
+    @UseGuards(ApplicationJwtGuard)
+    public async delete(
+        @Query() { id }: ModelDeleteRequest,
+        @Response({ passthrough: true }) res
+    ) {
+        let product = await this.productService.findOneBy({ id });
+        if (!product) throw new HttpException('Product not found.', 404);
+
+        await this.productService.remove(product);
+
+        res.status(200);
+    }
+}
