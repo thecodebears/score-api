fd8955737ba365ccb8332bdc8b34dbde960f9373
anore <66754841+codelumen@users.noreply.github.com>
2022-10-19 22:39:11 +0300

    Naming fix

--- src/models/connection/connection.service.ts
+++ src/models/connection/connection.service.ts
@@ -1,16 +1,12 @@
-import * as url from 'url';
 import { Injectable } from "@nestjs/common";
-import { HttpService } from '@nestjs/axios';
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Connection } from './connection.entity';
-import { EntityService } from '../base.service';
-import { AccountService } from '../account/account.service';
-import { Account } from '../account/account.entity';
+import { ModelService } from '../model.service';
 
 
 @Injectable()
-export class ConnectionService extends EntityService<Connection> {
+export class ConnectionService extends ModelService<Connection> {
     constructor(
         @InjectRepository(Connection) protected readonly repository: Repository<Connection>
     ) {
