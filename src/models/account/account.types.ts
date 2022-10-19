86cd107935e68c6eea67bf21f7f589e71d9e76ec
anore <66754841+codelumen@users.noreply.github.com>
2022-10-19 16:30:21 +0300

    Refactored types

--- src/models/account/account.types.ts
+++ src/models/account/account.types.ts
@@ -1,4 +1,3 @@
-import { IsString, IsNotEmpty, Allow, IsBoolean } from 'class-validator';
 import { Account } from "./account.entity";
 
 type ProductId = string;
@@ -8,38 +7,17 @@ export type Cart = {
     [key: ProductId]: Count
 };
 
-export class AccountIndexingDto {
-    @IsNotEmpty()
-    @IsString()
-    id: string;
-}
-
-export class GetAccountDto extends AccountIndexingDto {
-    @Allow()
-    fields: string
-}
-
-export class UpdateAccountDto extends AccountIndexingDto {
-    @IsString()
-    name?: string;
-
-    @IsString()
-    permissions?: string[];
-
-    @IsString()
-    public password?: string;
-
-    @IsString()
-    public avatarUrl?: string;
-
-    @IsBoolean()
-    public employee?: boolean;
-
-    @IsBoolean()
-    public admin?: boolean;
-}
-
-export interface RegistrationResponse {
+export type SignUpResponse = {
     account: Account,
     token: string
+};
+
+export class AccountSignUpRequest {
+    @IsString()
+    @IsNotNull()
+    username: string;
+
+    @IsString()
+    @IsNotNull()
+    password: string;
 }
