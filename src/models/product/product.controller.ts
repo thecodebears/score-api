ee914fa38dd1ec19b29b3efee7732140437a7bc9
anore <66754841+codelumen@users.noreply.github.com>
2022-10-20 18:56:19 +0300

    adapted to new permissions implementation, + add review method

--- src/models/product/product.controller.ts
+++ src/models/product/product.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, HttpException, Post, Query, UseGuards, Response } from "@nestjs/common";
+import { SetPermissions, Permissions } from "src/utils/permissions";
 import { ApplicationJwtGuard } from "../../guard/jwt.guard";
 import { ModelCreateRequest, ModelDeleteRequest, ModelGetRequest, ModelSearchRequest, ModelUpdateRequest } from "../model.types";
 import { Product } from "./product.entity";
 import { ProductService } from "./product.service";
-import { Permissions } from "../../decorators/permissions.decorator";
-import { ProductCreateRequest } from "./product.types";
+import { ProductAddReviewRequest, ProductCreateRequest } from "./product.types";
 
 
 @Controller('product')
 export class ProductController {
+    static {
+        Permissions.enlistPermissionsGroup('product', [ 'get', 'create', 'update', 'delete' ]);
+    }
+
     constructor(
         private productService: ProductService
     ) {}
 
     @Get()
-    @Permissions('product.get')
+    @SetPermissions('product.get')
     @UseGuards(ApplicationJwtGuard)
     public async get(@Query() { id, fields }: ModelGetRequest) {
         const product = await this.productService.findOneBy({ id });
@@ -23,14 +27,14 @@ export class ProductController {
     }
 
     @Post('create')
-    @Permissions('product.create')
+    @SetPermissions('product.create')
     @UseGuards(ApplicationJwtGuard)
     public async create(@Query() fields: ProductCreateRequest) {
         return this.productService.create(fields);
     }
 
     @Post('search')
-    @Permissions('product.search')
+    @SetPermissions('product.get')
     @UseGuards(ApplicationJwtGuard)
     public async search() {
         // Waiting for search engine.
@@ -38,7 +42,7 @@ export class ProductController {
     }
 
     @Post('update')
-    @Permissions('product.update')
+    @SetPermissions('product.update')
     @UseGuards(ApplicationJwtGuard)
     public async update(
         @Query() { id, ...overrideFields }: ModelUpdateRequest<Product>,
@@ -54,7 +58,7 @@ export class ProductController {
     }
 
     @Post('delete')
-    @Permissions('product.delete')
+    @SetPermissions('product.delete')
     @UseGuards(ApplicationJwtGuard)
     public async delete(
         @Query() { id }: ModelDeleteRequest,
@@ -66,5 +70,15 @@ export class ProductController {
         await this.productService.remove(product);
 
         res.status(200);
+    }
+
+    @Post('addReview')
+    @SetPermissions('product.update')
+    @UseGuards(ApplicationJwtGuard)
+    public async addReview(@Query() { id, author, rating, details }: ProductAddReviewRequest) {
+        let product = await this.productService.findOneBy({ id });
+        if (!product) throw new HttpException('Product not found.', 404);
+
+        return this.productService.addReview(product, { author, rating, details });
     }
 }
