a6d0c2cfc54318fefec253f2f51585aa0a383219
anore <66754841+codelumen@users.noreply.github.com>
2022-10-20 18:56:57 +0300

    + review entity import

--- src/models/product/product.module.ts
+++ src/models/product/product.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductService } from './product.service';
 import { Product } from './product.entity';
 import { ProductController } from './product.controller';
+import { Review } from './review/review.entity';
 
 
 @Module({
-    imports: [ TypeOrmModule.forFeature([ Product ]) ],
+    imports: [
+        TypeOrmModule.forFeature([ Product, Review ]),
+    ],
     providers: [ ProductService ],
     controllers: [ ProductController ],
     exports: [ ProductService ]
