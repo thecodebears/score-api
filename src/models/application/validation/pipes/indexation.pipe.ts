fccad6b6210036a8fcaf6f82be6f42fdd249c7ef
anore <66754841+codelumen@users.noreply.github.com>
2022-10-21 12:28:17 +0300

    + application indexation pipe

---
+++ src/models/application/validation/pipes/indexation.pipe.ts
@@ -0,0 +1,16 @@
+import { HttpException, Injectable, PipeTransform } from "@nestjs/common";
+import { ApplicationService } from "../../application.service";
+
+
+@Injectable()
+export class ApplicationIndexationPipe implements PipeTransform {
+    constructor(
+        private applicationService: ApplicationService
+    ) {}
+
+    public async transform(value: any) {
+        let application = await this.applicationService.findOneBy({ id: value as string});
+        if (!application) throw new HttpException('application.entity.notFound', 400);
+        return application;
+    }
+}
