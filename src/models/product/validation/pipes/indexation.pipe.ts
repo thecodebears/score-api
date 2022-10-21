1f3bd827525585c14d3e59c95db2a35ecc0a4bfc
anore <66754841+codelumen@users.noreply.github.com>
2022-10-21 12:28:23 +0300

    + product indexation pipe

---
+++ src/models/product/validation/pipes/indexation.pipe.ts
@@ -0,0 +1,16 @@
+import { HttpException, Injectable, PipeTransform } from "@nestjs/common";
+import { ProductService } from "../../product.service";
+
+
+@Injectable()
+export class ProductIndexationPipe implements PipeTransform {
+    constructor(
+        private productService: ProductService
+    ) {}
+
+    public async transform(value: any) {
+        let product = await this.productService.findOneBy({ id: value as string});
+        if (!product) throw new HttpException('product.entity.notFound', 400);
+        return product;
+    }
+}
