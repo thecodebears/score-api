f91a2099de425481d53cb287a687fd57e83876d3
anore <66754841+codelumen@users.noreply.github.com>
2022-10-21 12:25:04 +0300

    - unused imports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,5 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AccountJwtGuard, ApplicationJwtGuard } from './guards/jwt.guard';
-import { AdminGuard } from './guards/admin.guard';
-import { PermissionsGuard } from './guards/permissions.guard';
 
 
 @Controller()
