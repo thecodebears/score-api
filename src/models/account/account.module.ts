18bb8b12b03f569d040314446711494684437bc7
anore <66754841+codelumen@users.noreply.github.com>
2022-10-21 12:27:10 +0300

    + imports & providers

--- src/models/account/account.module.ts
+++ src/models/account/account.module.ts
@@ -1,21 +1,32 @@
-import {Global, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller'
 import { Account } from './account.entity';
 import { JwtModule } from '@nestjs/jwt';
 import env from 'environment';
+import { Connection } from './connection/connection.entity';
+import { DiscordStrategy } from 'src/security/strategies/discord.strategy';
+import { SessionSerializer } from 'src/utils/serializer/serializer';
+import { ConnectionService } from './connection/connection.service';
+import { SecurityModule } from 'src/security/security.module';
 
 
 @Global()
 @Module({
     imports: [
-        TypeOrmModule.forFeature([ Account ]),
+        TypeOrmModule.forFeature([ Account, Connection ]),
         JwtModule.register({
             secret: env.security.jwt.secret
-        })
+        }),
+        SecurityModule
     ],
-    providers: [ AccountService ],
+    providers: [
+        AccountService,
+        ConnectionService,
+        DiscordStrategy,
+        SessionSerializer
+    ],
     controllers: [ AccountController ],
     exports: [ AccountService ]
 })
