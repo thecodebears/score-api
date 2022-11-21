ac9339eb7fd35b0ba8e912fcd8cee0c88e7c209f
anore <66754841+codelumen@users.noreply.github.com>
2022-11-21 19:52:21 +0300

    modified Product fields

--- src/models/product/product.entity.ts
+++ src/models/product/product.entity.ts
@@ -1,37 +1,48 @@
 import { Column, Entity } from 'typeorm';
 import { ModelEntity } from '../model.entity';
-import { ProductFeatures, ProductReview } from './product.types';
+import { ProductFeatures } from './product.types';
 
 
 @Entity()
 export abstract class Product extends ModelEntity {
+    @Column('varchar', { length: 4, default: 'A000' })
+    public code: string;
+
     @Column('varchar', { length: 256 })
     public label: string;
 
-    @Column('varchar', { length: 256 })
-    public sublabel: string;
-
     @Column('varchar')
     public description: string;
-
-    @Column('jsonb', { default: [] })
-    public reviews: ProductReview[];
 
     @Column('varchar')
     public category: string;
 
     @Column('int')
-    public price: number;
+    public costPrice: number;
+
+    @Column('float')
+    public markup: number;
 
     @Column('int')
     public quantity: number;
 
-    @Column('int', { default: 0 })
-    public sold: number;
-
-    @Column('boolean')
-    public delivered: boolean;
-
     @Column('jsonb')
     public features: ProductFeatures;
+
+    @Column('varchar', { array: true })
+    public tags: string[];
+
+    @Column('varchar', { array: true, nullable: true })
+    public photos: string[];
+
+    // analytics
+
+    @Column('int', { default: 0 })
+    public sales: number;
+
+    @Column('int', { default: 0 })
+    public pins: number;
+
+    @Column('int', { default: 0 })
+    public views: number;
 }
