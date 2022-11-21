09442e3369988c0b960cad2822b90f0daae42f57
anore <66754841+codelumen@users.noreply.github.com>
2022-11-21 19:50:44 +0300

    + ProductCodeIndexationPipe

--- src/models/product/validation/pipes/indexation.pipe.ts
+++ src/models/product/validation/pipes/indexation.pipe.ts
@@ -9,7 +9,20 @@ export class ProductIndexationPipe implements PipeTransform {
     ) {}
 
     public async transform(value: any) {
-        let product = await this.productService.findOneBy({ id: value as string});
+        let product = await this.productService.findOneBy({ id: value as string });
+        if (!product) throw new HttpException('product.entity.notFound', 400);
+        return product;
+    }
+}
+
+@Injectable()
+export class ProductCodeIndexationPipe implements PipeTransform {
+    constructor(
+        private productService: ProductService
+    ) {}
+
+    public async transform(value: any) {
+        let product = await this.productService.findOneBy({ code: value as string });
         if (!product) throw new HttpException('product.entity.notFound', 400);
         return product;
     }
