5730d426503c3a9f271e3948133eddcf867cc811
anore <66754841+codelumen@users.noreply.github.com>
2022-11-21 19:52:06 +0300

    - addReview (deprecated)

--- src/models/product/product.service.ts
+++ src/models/product/product.service.ts
@@ -17,25 +17,4 @@ export class ProductService extends ModelService<Product> {
     ) {
         super();
     }
-
-    public async addReview(product: Product, { author, rating, details }: Columns<Review>): Promise<Review> {
-        if (product.reviews.some(r => r.author === author))
-            throw new HttpException('This account already has already reviewed this product.', 400);
-        if (rating > 10 || rating < 1)
-            throw new HttpException('Exceeded rating number range. (0 -> 10)', 400);
-        if (!(await this.accountService.findOneBy({ id: author })))
-            throw new HttpException('Author not found.', 404);
-
-        let review = await this.reviewRepository.create({ author, rating, details });
-        let existingReviews = product.reviews;
-
-        existingReviews.push({
-            review: review.id,
-            author: author
-        });
-
-        await this.update(product, { reviews: existingReviews });
-
-        return review;
-    }
 }
