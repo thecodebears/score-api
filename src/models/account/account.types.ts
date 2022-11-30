28498e335b1be07030d3d6403b71ac968fdce785
anore <66754841+codelumen@users.noreply.github.com>
2022-11-30 17:55:39 +0300

    - unused classes

--- src/models/account/account.types.ts
+++ src/models/account/account.types.ts
@@ -3,29 +3,12 @@ import { IsString, IsNotEmpty, IsInt } from "class-validator";
 import { Account } from "./account.entity";
 
 
-export type CartItem = {
-    id: number,
-    count: number
-};
+export class CartItem {
+    id: number;
+    count: number;
+}
 
 export type SignUpResponse = {
     account: Account,
     token: string
 };
-
-export class AccountSignUpRequest {
-    @IsString()
-    @IsNotEmpty()
-    username: string;
-
-    @IsString()
-    @IsNotEmpty()
-    password: string;
-}
-
-export class AccountAddToCartRequest {
-    @IsInt()
-    @IsNotEmpty()
-    @Transform(({ value }) => parseInt(value))
-    count: number;
-}
