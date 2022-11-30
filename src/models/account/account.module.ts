e2b3172cef86fd47642ed3482654e1918396f0d4
anore <66754841+codelumen@users.noreply.github.com>
2022-11-30 17:57:02 +0300

    linking

--- src/models/account/account.module.ts
+++ src/models/account/account.module.ts
@@ -10,27 +10,33 @@ import { DiscordStrategy } from 'src/security/strategies/discord.strategy';
 import { SessionSerializer } from 'src/utils/serializer/serializer';
 import { ConnectionService } from './connection/connection.service';
 import { SecurityModule } from 'src/security/security.module';
-import { ProductService } from '../product/product.service';
-import { ProductModule } from '../product/product.module';
+import { ItemModule } from '../item/item.module';
+import { OrderService } from './order/order.service';
+import { OrderController } from './order/order.controller';
+import { Order } from './order/order.entity';
 
 
 @Global()
 @Module({
     imports: [
-        TypeOrmModule.forFeature([ Account, Connection ]),
+        TypeOrmModule.forFeature([ Account, Connection, Order ]),
         JwtModule.register({
             secret: env.security.jwt.secret
         }),
         SecurityModule,
-        ProductModule
+        ItemModule
     ],
     providers: [
         AccountService,
         ConnectionService,
+        OrderService,
         DiscordStrategy,
         SessionSerializer
     ],
-    controllers: [ AccountController ],
+    controllers: [
+        AccountController,
+        OrderController
+    ],
     exports: [ AccountService ]
 })
 export class AccountModule {}
